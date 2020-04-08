6c816cbf6703dea5fd3f7d648a2d57f9c49b2860
Manuel Gieseking <mgieseking@users.noreply.github.com>
2020-04-08 19:30:23 +0200

    MG: new version of the PN consturction of a pnwt without initial places (at least for the inhibitor versions)

--- src/uniolunisaar/adam/util/logics/LogicsTools.java
+++ src/uniolunisaar/adam/util/logics/LogicsTools.java
@@ -8,6 +8,12 @@ import uniol.apt.adt.pn.Transition;
 import uniolunisaar.adam.ds.logics.FormulaBinary;
 import uniolunisaar.adam.ds.logics.IFormula;
 import uniolunisaar.adam.ds.logics.IOperatorBinary;
+import uniolunisaar.adam.ds.logics.ctl.CTLFormula;
+import uniolunisaar.adam.ds.logics.ctl.CTLOperators;
+import uniolunisaar.adam.ds.logics.ctl.ICTLFormula;
+import uniolunisaar.adam.ds.logics.ctl.flowctl.FlowCTLFormula;
+import uniolunisaar.adam.ds.logics.ctl.flowctl.RunCTLFormula;
+import uniolunisaar.adam.ds.logics.ctl.flowctl.RunCTLOperators;
 import uniolunisaar.adam.ds.logics.ltl.ILTLFormula;
 import uniolunisaar.adam.ds.logics.ltl.LTLFormula;
 import uniolunisaar.adam.ds.logics.ltl.LTLOperators;
@@ -30,7 +36,7 @@ public class LogicsTools {
         OUTGOING
     }
 
-    public static List<FlowLTLFormula> getFlowFormulas(IFormula formula) {
+    public static List<FlowLTLFormula> getFlowLTLFormulas(IFormula formula) {
         List<FlowLTLFormula> flowFormulas = new ArrayList<>();
         if (formula instanceof FlowLTLFormula) {
             flowFormulas.add((FlowLTLFormula) formula);
@@ -38,11 +44,28 @@ public class LogicsTools {
         } else if (formula instanceof ILTLFormula) {
             return flowFormulas;
         } else if (formula instanceof RunLTLFormula) {
-            return getFlowFormulas(((RunLTLFormula) formula).getPhi());
+            return getFlowLTLFormulas(((RunLTLFormula) formula).getPhi());
         } else if (formula instanceof FormulaBinary<?, ?, ?>) {
             FormulaBinary<?, ?, ?> binF = (FormulaBinary<?, ?, ?>) formula;
-            flowFormulas.addAll(getFlowFormulas(binF.getPhi1()));
-            flowFormulas.addAll(getFlowFormulas(binF.getPhi2()));
+            flowFormulas.addAll(getFlowLTLFormulas(binF.getPhi1()));
+            flowFormulas.addAll(getFlowLTLFormulas(binF.getPhi2()));
+        }
+        return flowFormulas;
+    }
+
+    public static List<FlowCTLFormula> getFlowCTLFormulas(IFormula formula) {
+        List<FlowCTLFormula> flowFormulas = new ArrayList<>();
+        if (formula instanceof FlowCTLFormula) {
+            flowFormulas.add((FlowCTLFormula) formula);
+            return flowFormulas;
+        } else if (formula instanceof ICTLFormula) {
+            return flowFormulas;
+        } else if (formula instanceof RunCTLFormula) {
+            return getFlowCTLFormulas(((RunCTLFormula) formula).getPhi());
+        } else if (formula instanceof FormulaBinary<?, ?, ?>) {
+            FormulaBinary<?, ?, ?> binF = (FormulaBinary<?, ?, ?>) formula;
+            flowFormulas.addAll(getFlowCTLFormulas(binF.getPhi1()));
+            flowFormulas.addAll(getFlowCTLFormulas(binF.getPhi2()));
         }
         return flowFormulas;
     }
@@ -71,16 +94,16 @@ public class LogicsTools {
         return (!fairness.toString().equals("TRUE")) ? new LTLFormula(fairness, LTLOperators.Binary.IMP, formula) : formula;
     }
 
-    public static ILTLFormula convert(IFormula f) throws NotConvertableException {
+    public static ILTLFormula convert2LTL(IFormula f) throws NotConvertableException {
         if (f instanceof IFlowFormula) {
             throw new NotConvertableException("The formula contains a flow formula '" + f.toSymbolString() + "'. Hence, we cannot transform it to an LTL formula.");
         } else if (f instanceof RunLTLFormula) {
-            return new LTLFormula(convert(((RunLTLFormula) f).getPhi()));
+            return new LTLFormula(convert2LTL(((RunLTLFormula) f).getPhi()));
         } else if (f instanceof IRunFormula && f instanceof FormulaBinary) {
             FormulaBinary<?, ?, ?> form = ((FormulaBinary<?, ?, ?>) f);
             IOperatorBinary<?, ?> op = form.getOp();
-            ILTLFormula f1 = convert(form.getPhi1());
-            ILTLFormula f2 = convert(form.getPhi2());
+            ILTLFormula f1 = convert2LTL(form.getPhi1());
+            ILTLFormula f2 = convert2LTL(form.getPhi2());
             if (op instanceof RunOperators.Binary) {
                 if (op.equals(RunOperators.Binary.AND)) {
                     return new LTLFormula(f1, LTLOperators.Binary.AND, f2);
@@ -93,6 +116,33 @@ public class LogicsTools {
             throw new RuntimeException("Not every possible case matched.");
         } else {
             return (ILTLFormula) f;
+        }
+    }
+
+    public static ICTLFormula convert2CTL(IFormula f) throws NotConvertableException {
+        if (f instanceof IFlowFormula) {
+            throw new NotConvertableException("The formula contains a flow formula '" + f.toSymbolString() + "'. Hence, we cannot transform it to a CTL formula.");
+        } else if (f instanceof RunCTLFormula) {
+            return new CTLFormula(convert2CTL(((RunCTLFormula) f).getPhi()));
+        } else if (f instanceof IRunFormula && f instanceof FormulaBinary) {
+            FormulaBinary<?, ?, ?> form = ((FormulaBinary<?, ?, ?>) f);
+            IOperatorBinary<?, ?> op = form.getOp();
+            ICTLFormula f1 = convert2CTL(form.getPhi1());
+            ICTLFormula f2 = convert2CTL(form.getPhi2());
+            if (op instanceof RunCTLOperators.Binary) {
+                if (op.equals(RunCTLOperators.Binary.AND)) {
+                    return new CTLFormula(f1, CTLOperators.Binary.AND, f2);
+                } else if (op.equals(RunCTLOperators.Binary.OR)) {
+                    return new CTLFormula(f1, CTLOperators.Binary.OR, f2);
+                } else if (op.equals(RunCTLOperators.Binary.IMP)) {
+                    return new CTLFormula(f1, CTLOperators.Binary.IMP, f2);
+                } else if (op.equals(RunCTLOperators.Binary.BIMP)) {
+                    return new CTLFormula(f1, CTLOperators.Binary.BIMP, f2);
+                }
+            }
+            throw new RuntimeException("Not every possible case matched.");
+        } else {
+            return (ICTLFormula) f;
         }
     }
 
